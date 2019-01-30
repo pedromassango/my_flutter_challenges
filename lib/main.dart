3cf3568903c8cb55fcecd38e0bb57ae73cd98918
pedromassango <pedromassango.developer@gmail.com>
2019-01-31 00:05:40 +0100

    path experiment

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,61 @@ class SplashPage extends StatelessWidget{
       body: Container(
         color: Colors.white,
         child: Center(
-          child: Text('Main',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 32
-            ),
+          child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: ClipPath(
+                    clipper: MyClipper(align: Alignment.bottomLeft),
+                    child: Container(
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: ClipPath(
+                    clipper: MyClipper(align: Alignment.bottomRight),
+                    child: Container(
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                )
+              ],
           ),
         ),
       ),
     );
   }
+}
+// Love song (Adele)
+
+class MyClipper extends CustomClipper<Path>{
+
+   Alignment align;
+
+  MyClipper({this.align});
+
+
+  @override
+  Path getClip(Size size) {
+    var p = Path();
+
+
+
+    Rect rect;
+    if(align == Alignment.bottomLeft){
+      rect = Rect.fromLTWH(-40, size.height-55, 100, 100);
+    }else{
+      rect = Rect.fromLTWH(size.width-40, size.height-60, 100, 100);
+    }
+
+    p.addOval(rect);
+
+
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
