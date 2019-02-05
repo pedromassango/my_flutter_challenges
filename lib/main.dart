08b54416966053f732b935cee2a4664ee903faec
pedromassango <pedromassango.developer@gmail.com>
2019-02-05 20:56:55 +0100

    transformation experiments

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,96 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp( MainAppWidget());
+  runApp(MainAppWidget());
 }
 
-class MainAppWidget extends StatelessWidget{
-
+class MainAppWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Main App Widget',
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
+      theme: ThemeData(primarySwatch: Colors.red),
       home: SplashPage(),
     );
   }
 }
 
-class SplashPage extends StatelessWidget{
+class SplashPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Stack(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: ClipPath(
-                    clipper: MyClipper(align: Alignment.bottomLeft),
-                    child: Container(
-                      color: Colors.red,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: ClipPath(
-                    clipper: MyClipper(align: Alignment.bottomRight),
-                    child: Container(
-                      color: Colors.deepPurple,
-                    ),
-                  ),
-                )
-              ],
-          ),
-        ),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return MyState();
   }
 }
 // Love song (Adele)
 
-class MyClipper extends CustomClipper<Path>{
-
-   Alignment align;
-
-  MyClipper({this.align});
-
+class MyState extends State<SplashPage> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> angle;
 
   @override
-  Path getClip(Size size) {
-    var p = Path();
-
-
-
-    Rect rect;
-    if(align == Alignment.bottomLeft){
-      rect = Rect.fromLTWH(-40, size.height-55, 100, 100);
-    }else{
-      rect = Rect.fromLTWH(size.width-40, size.height-60, 100, 100);
-    }
-
-    p.addOval(rect);
-
-
-    return p;
+  void initState() {
+    // TODO: implement initState
+    super.initState();
   }
 
   @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Hello There",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (c, index) {
+          return _buildItem(index);
+        },
+      ),
+    );
+  }
+}
+
+Widget _buildItem(int index) {
+  List<Color> _colors = [
+    Colors.blue,
+    Colors.green,
+    Colors.red,
+    Colors.purple,
+    Colors.deepPurple
+  ];
+
+  return Transform(
+    transform: Matrix4.identity()
+      ..setEntry(3, 2, 0.01)
+      ..rotateX(0.2),
+    alignment: FractionalOffset.center,
+    child: Container(
+      margin: EdgeInsets.fromLTRB(32, 0, 32, 0),
+      padding: EdgeInsets.fromLTRB(16, 32, 16, 8),
+      decoration: BoxDecoration(
+          color: _colors.elementAt(index),
+          borderRadius: BorderRadius.all(Radius.circular(16))),
+      height: 150,
+      width: 100,
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Icon(Icons.place, color: Colors.white,),
+              ),
+              Text('Time Square', style: TextStyle(color: Colors.white),),
+              Spacer(),
+              Text('${index}0%', style: TextStyle(color: Colors.white),)
+            ],
+          )
+        ],
+      ),
+    ),
+  );
 }
