e414cdf44e981b912b9a396f6cffbf35e6da88fc
Pedro Massango <pedromassango.developer@gmail.com>
2020-06-24 14:31:29 +0100

    Added support for Flutter MacOS

---
+++ android/app/src/main/kotlin/com/pedromassango/my_flutter_challenges/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pedromassango.my_flutter_challenges
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_challenges/coffee_app_auth.dart';
 
-void main() => runApp( MyApp());
+void main() => runApp(MyApp());
